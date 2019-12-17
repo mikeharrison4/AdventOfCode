76bf299ac1b6d771638094f73922b033e89ff0b4
mikeyh-aexp <55436448+mikeyh-aexp@users.noreply.github.com>
2019-12-17 09:59:48 +0000

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,50 @@
+public static void main(String[] args) throws IOException {
+
+        getNoOfOrbits("COM)B\n" +
+                "B)C\n" +
+                "C)D\n" +
+                "D)E\n" +
+                "E)F\n" +
+                "B)G\n" +
+                "G)H\n" +
+                "D)I\n" +
+                "E)J\n" +
+                "J)K\n" +
+                "K)L");
+
+    }
+
+    private static void getNoOfOrbits(String mapList) {
+
+        Map<String, List<String>> hashMap = new HashMap<>();
+        String[] mapListSplit = mapList.split("\n");
+
+        for (String s : mapListSplit) {
+
+            String[] elem = s.split("\\)");
+            if( !hashMap.containsKey(elem[1]) ) {
+                hashMap.put(elem[1], new ArrayList<>());
+            }
+            hashMap.get(elem[1]).add(elem[0]);
+
+        }
+
+        System.out.println(hashMap.toString());
+
+        int sum = 0;
+        for (String key : hashMap.keySet()) {
+            sum += ancestors(hashMap, key).size();
+        }
+        System.out.println(sum);
+
+    }
+
+    private static List<String> ancestors(Map<String, List<String>> hashMap, String key) {
+        List<String> nodes = new ArrayList<>();
+        System.out.println(key);
+        while(!key.equals("COM")) {
+            key = hashMap.get(key).get(0);
+            nodes.add(key);
+        }
+        return nodes;
+    }
